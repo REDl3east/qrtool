619575d16a289d5625b8f2538ad1a535225eb147
Dalton Overmyer <overmyerms13dalton@gmail.com>
2022-11-08 18:38:08 -0600

    added qr code library and minimal sdl2 app.

--- main.c
+++ main.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
 
+#include "SDL.h"
+
+static const char* APP_NAME = "SDL2 QR Code";
+static int INITIAL_WIDTH    = 1280;
+static int INITIAL_HEIGHT   = 720;
+
 int main(int argv, char** argc) {
-  printf("hello world!\n");
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("[ERROR] %s\n", SDL_GetError());
+    return 1;
+  }
+
+  Uint32 flags       = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
+  SDL_Window* window = SDL_CreateWindow(APP_NAME, INITIAL_WIDTH * 0.25, INITIAL_HEIGHT * 0.25, INITIAL_WIDTH, INITIAL_HEIGHT, flags);
+
+  if (!window) {
+    printf("[ERROR] SDL_CreateWindow: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (!renderer) {
+    printf("[ERROR] SDL_CreateRenderer: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  if (SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND) < 0) {
+    printf("[ERROR] SDL_SetRenderDrawBlendMode: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  int done = 0;
+
+  while (!done) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if ((event.type == SDL_QUIT) ||
+          (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_q) ||
+          (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))) {
+        done = 1;
+      }
+      switch (event.type) {
+        case SDL_KEYDOWN: {
+          if (event.key.keysym.sym == SDLK_1) {
+            printf("%d\n", 0);
+          } else if (event.key.keysym.sym == SDLK_2) {
+            printf("%d\n", 1);
+          } else if (event.key.keysym.sym == SDLK_3) {
+            printf("%d\n", 2);
+          }
+
+          break;
+        }
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 125, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_Quit();
+
   return 0;
 }
