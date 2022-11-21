e5558a7ecbba8cb08883c57ee11d73a3af819206
overmyerD <dalton@raptorpowersystems.com>
2022-11-21 15:57:26 -0600

    started getting text input via stdin or -t option

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "SDL.h"
-#include "qrcodegen.h"
-
 #include "SDL_image.h"
+#include "argtable3.h"
+#include "qrcodegen.h"
 
 static const char* APP_NAME = "SDL2 QR Code";
 static int INITIAL_WIDTH    = 1280;
 static int INITIAL_HEIGHT   = 720;
 
+#define MAX_TEXT_INPUT 1024
 typedef struct QrAttr {
   char* input;
   enum qrcodegen_Ecc level;
@@ -42,9 +46,103 @@ int createQrCodeSurfaceScale(QrSurface* qr_surface, float scale);
 int createQrCodeSurfaceWithSize(QrSurface* qr_surface, int size);
 void destroyQrCodeSurface(QrSurface* qr_surface);
 
-int main(int argv, char** argc) {
+// https://stackoverflow.com/questions/41869803/what-is-the-best-alternative-to-strncpy by chqrlie
+char* safe_strcpy(char* dest, size_t size, const char* src) {
+  if (size > 0) {
+    size_t i;
+    for (i = 0; i < size - 1 && src[i]; i++) {
+      dest[i] = src[i];
+    }
+    dest[i] = '\0';
+  }
+  return dest;
+}
+
+int read_stdin(char* buffer, size_t max_size) {
+  int total_bytes_read = 0;
+  int nbytes_read      = 0;
+
+  while (1) {
+    nbytes_read = read(0, buffer + total_bytes_read, max_size - total_bytes_read);
+    total_bytes_read += nbytes_read;
+
+    if (nbytes_read == 0) {
+      buffer[total_bytes_read] = '\0';
+      break;
+    } else if (total_bytes_read >= max_size) {
+      buffer[max_size] = '\0';
+
+      // flush stdin so it doesn't pollute the terminal
+      int c;
+      while ((c = getchar()) != '\n' && c != EOF) {
+      }
+      break;
+    }
+
+    if (nbytes_read < 0) return -1;
+  }
+  return total_bytes_read;
+}
+
+int main(int argc, char** argv) {
+  struct arg_str* text_input_arg = arg_str0("t", "text-input", "INPUT", "The input text that will be used to generate the QR code");
+  // struct arg_lit* recurse  = arg_lit0("R", NULL, "recurse through subdirectories");
+  // struct arg_int* repeat   = arg_int0("k", "scalar", NULL, "define scalar value k (default is 3)");
+  // struct arg_str* defines  = arg_strn("D", "define", "MACRO", 0, argc + 2, "macro definitions");
+  // struct arg_file* outfile = arg_file0("o", NULL, "<output>", "output file (default is \"-\")");
+  // struct arg_lit* verbose  = arg_lit0("v", "verbose,debug", "verbose messages");
+  struct arg_lit* help    = arg_lit0(NULL, "help", "print this help and exit");
+  struct arg_lit* version = arg_lit0(NULL, "version", "print version information and exit");
+  // struct arg_file* infiles = arg_filen(NULL, NULL, NULL, 1, argc + 2, "input file(s)");
+  struct arg_end* end = arg_end(20);
+  void* argtable[]    = {text_input_arg, help, version, end};
+
+  if (arg_nullcheck(argtable) != 0) {
+    printf("%s: insufficient memory\n", argv[0]);
+    return 1;
+  }
+
+  int err = arg_parse(argc, argv, argtable);
+
+  if (help->count > 0) {
+    printf("Usage: %s", argv[0]);
+    arg_print_syntax(stdout, argtable, "\n");
+    printf("This program generate QR Codes.\n");
+
+    arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+    return 0;
+  }
+
+  if (version->count > 0) {
+    printf("'%s' This program generate QR Codes.\n", argv[0]);
+    printf("2022, Dalton Overmyer\n");
+    return 0;
+  }
+
+  if (err > 0) {
+    arg_print_errors(stdout, end, argv[0]);
+    return 1;
+  }
+
+  char text_input[MAX_TEXT_INPUT];
+
+  if (text_input_arg->count > 0) {
+    safe_strcpy(text_input, MAX_TEXT_INPUT, text_input_arg->sval[0]);
+    if (text_input == NULL) {
+      fprintf(stderr, "[ERROR] safe_strcpy failed\n");
+      return 1;
+    }
+  } else {
+    if (read_stdin(text_input, MAX_TEXT_INPUT) < 0) {
+      fprintf(stderr, "[ERROR] read failed: %s\n", strerror(errno));
+      return 1;
+    }
+  }
+
+  printf("%s\n", text_input);
+
   QrSurface qr_surface;
-  qr_surface.qr.attr.input       = "What are you looking at?";
+  qr_surface.qr.attr.input       = text_input;
   qr_surface.qr.attr.level       = qrcodegen_Ecc_HIGH;
   qr_surface.qr.attr.mask        = qrcodegen_Mask_AUTO;
   qr_surface.qr.attr.version_min = 1;
@@ -142,6 +240,8 @@ int main(int argv, char** argc) {
 
     SDL_RenderPresent(renderer);
   }
+
+  arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
 
   destroyQrCodeSurface(&qr_surface);
   SDL_DestroyRenderer(renderer);
